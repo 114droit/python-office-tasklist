82b031dedb2e38352c323efd445811db8c9bb79a
144droit <indieohren@gmail.com>
2024-10-04 15:35:20 +0200

    created function to show tasklist

--- office-tasklist.py
+++ office-tasklist.py
@@ -5,10 +5,18 @@ tasklist = []
 def add_task():
     task = input("Was hast du zu erledigen?")                   # Nutzer wird gefragt: 'Was hast du zu erledigen?' und die Eingabe wird in der Variablen task gespeichert
     if task:                                                    # Wenn Eingabe vorhanden
-        date = input("Bis wann?")                               # Nutzer wird gefragt: 'Bis wann?' und die Eingabe wird in der Variablen date gespeichert
+        date = input("Bis zu welchem Datum?")                   # Nutzer wird gefragt: 'Bis wann?' und die Eingabe wird in der Variablen date gespeichert
         tasklist.append(f"{task} bis {date}")                   # Wert von task, bis und Wert von date werden als ein String der Liste tasklist hinzugefügt
         print(f"{task} wurde der Aufgabenliste hinzugefügt")    # Gib aus: '(Wert von task) wurde der Aufgabenliste hinzugefügt
     else:                                                       # Andernfalls
-        print("Nichts hinzugefügt")                             # Gib aus: 'Nichts hinzugefügt'
+        print("Der Aufgabenliste wurde nichts hinzugefügt")                             # Gib aus: 'Nichts hinzugefügt'
 add_task()
-print(tasklist)
+
+# Definieren der show_tasklist Funktion
+def show_tasklist():
+    if not tasklist:
+        print("Deine Aufgabenliste ist leer")
+    else:
+        for x in tasklist:
+            print(x)
+show_tasklist()
